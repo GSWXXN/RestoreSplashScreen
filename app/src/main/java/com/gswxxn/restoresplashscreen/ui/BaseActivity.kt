94b4cc1ed77307dc45b45695f0091c7aa59bb74b
GSWXXN <819269088@qq.com>
2022-05-22 03:02:09 -0400

    新增 隐藏功能描述 & 微调布局

--- app/src/main/java/com/gswxxn/restoresplashscreen/ui/BaseActivity.kt
+++ app/src/main/java/com/gswxxn/restoresplashscreen/ui/BaseActivity.kt
@@ -2,9 +2,17 @@ package com.gswxxn.restoresplashscreen.ui
 
 import android.app.Activity
 import android.os.Bundle
+import android.view.View
 import com.gswxxn.restoresplashscreen.R
 
 abstract class BaseActivity : Activity() {
+
+    open fun showView(isShow: Boolean = true, vararg views: View?) {
+        for (element in views) {
+            element?.visibility = if (isShow) View.VISIBLE else View.GONE
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         actionBar?.hide()

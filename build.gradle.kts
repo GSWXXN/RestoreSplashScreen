296e97ee9dd3d6cdc4973cf2d01338533e7f778b
ZQDesigned <89234036+ZQDesigned@users.noreply.github.com>
2022-07-14 16:53:23 +0800

    Update kotlin plugin & ksp version (#12)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.android.application") version "7.2.1" apply false
-    kotlin("jvm") version "1.7.0" apply false
+    kotlin("jvm") version "1.7.10" apply false
 }
 
 tasks.register("clean", Delete::class) {

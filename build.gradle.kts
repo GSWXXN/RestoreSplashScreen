4e32f01e0385fecf409663c19f987893426f9eb6
ZOUQUAN-A <89234036+ZQDesigned@users.noreply.github.com>
2022-08-07 01:01:07 +0800

    Update AGP to 7.2.2
    Repair .gitignore file at the same time, in order to avoid update .idea/misc.xml in every commit.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.android.application") version "7.2.1" apply false
+    id("com.android.application") version "7.2.2" apply false
     kotlin("jvm") version "1.7.10" apply false
 }
 

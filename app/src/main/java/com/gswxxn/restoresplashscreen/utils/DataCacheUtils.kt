6ffe42be22ffc65107ecb6d3deb01aa0d27f1560
GSWXXN <819269088@qq.com>
2022-10-12 07:45:46 -0400

    fix failed to get night mode

--- app/src/main/java/com/gswxxn/restoresplashscreen/utils/DataCacheUtils.kt
+++ app/src/main/java/com/gswxxn/restoresplashscreen/utils/DataCacheUtils.kt
@@ -5,9 +5,17 @@ import android.graphics.drawable.Drawable
 object DataCacheUtils {
     var iconData = HashMap<String, Drawable>()
     var colorData = HashMap<String, Int?>()
+    private var isDarkModeCache = false
 
     fun clear() {
         iconData.clear()
         colorData.clear()
+    }
+
+    fun checkDarkModeChanged(currentDarkMode: Boolean) {
+        if (currentDarkMode != isDarkModeCache) {
+            colorData.clear()
+            isDarkModeCache = currentDarkMode
+        }
     }
 }

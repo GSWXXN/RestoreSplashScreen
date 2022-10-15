3e98b76b1b208304c977fefe20f31424c75400db
GSWXXN <34705923+GSWXXN@users.noreply.github.com>
2022-10-15 13:18:35 -0400

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,6 +24,18 @@ android {
         }
     }
 
+    flavorDimensionList.add("tier")
+    productFlavors {
+        create("CI") {
+            dimension = "tier"
+            versionCode = defaultConfig.versionCode?.plus(1)
+            versionName = "${defaultConfig.versionName?.split(" - ")?.get(0)}${getGitHeadRefsSuffix(rootProject)}"
+        }
+        create("Release-apk") {
+            dimension = "tier"
+        }
+    }
+
     buildFeatures {
         viewBinding = true
     }
@@ -59,3 +71,27 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
 }
 
+// from https://github.com/qqlittleice/MiuiHome_R/blob/main/app/build.gradle.kts
+fun getGitHeadRefsSuffix(project: Project): String {
+    // .git/HEAD描述当前目录所指向的分支信息，内容示例："ref: refs/heads/master\n"
+    val headFile = File(project.rootProject.projectDir, ".git" + File.separator + "HEAD")
+    if (headFile.exists()) {
+        val string: String = headFile.readText(Charsets.UTF_8)
+        val string1 = string.replace(Regex("""ref:|\s"""), "")
+        val result = if (string1.isNotBlank() && string1.contains('/')) {
+            val refFilePath = ".git" + File.separator + string1
+            // 根据HEAD读取当前指向的hash值，路径示例为：".git/refs/heads/master"
+            val refFile = File(project.rootProject.projectDir, refFilePath)
+            // 索引文件内容为hash值+"\n"，
+            // 示例："90312cd9157587d11779ed7be776e3220050b308\n"
+            refFile.readText(Charsets.UTF_8).replace(Regex("""\s"""), "").subSequence(0, 7)
+        } else {
+            string.substring(0, 7)
+        }
+        println("commit_id: $result")
+        return "-CI.$result"
+    } else {
+        println("WARN: .git/HEAD does NOT exist")
+        return ""
+    }
+}

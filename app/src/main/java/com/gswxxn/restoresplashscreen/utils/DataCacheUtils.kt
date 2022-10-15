682be854a78bbfac3e3fb6e958ebafbcf65edeb1
GSWXXN <34705923+GSWXXN@users.noreply.github.com>
2022-10-15 14:01:53 -0400

    Update DataCacheUtils.kt

--- app/src/main/java/com/gswxxn/restoresplashscreen/utils/DataCacheUtils.kt
+++ app/src/main/java/com/gswxxn/restoresplashscreen/utils/DataCacheUtils.kt
@@ -5,17 +5,9 @@ import android.graphics.drawable.Drawable
 object DataCacheUtils {
     var iconData = HashMap<String, Drawable>()
     var colorData = HashMap<String, Int?>()
-    private var isDarkModeCache = false
 
     fun clear() {
         iconData.clear()
         colorData.clear()
-    }
-
-    fun checkDarkModeChanged(currentDarkMode: Boolean) {
-        if (currentDarkMode != isDarkModeCache) {
-            colorData.clear()
-            isDarkModeCache = currentDarkMode
-        }
     }
 }

13e68ec00a77151ebaaaaaed94edc22b956b2ace
hosizoraru <63982994+hosizoraru@users.noreply.github.com>
2022-10-16 02:04:06 +0800

    添加 CI 构建 (#20)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,9 @@
+import java.util.Properties
+
 plugins {
     id("com.android.application")
     kotlin("android")
-    id("com.google.devtools.ksp") version "1.7.10-1.0.6"
+    id("com.google.devtools.ksp") version "1.7.20-1.0.7"
 }
 
 android {
@@ -16,11 +18,41 @@ android {
         versionName = "2.4"
     }
 
+    val properties = Properties()
+    runCatching { properties.load(project.rootProject.file("local.properties").inputStream()) }
+    val keystorePath = properties.getProperty("KEYSTORE_PATH") ?: System.getenv("KEYSTORE_PATH")
+    val keystorePwd = properties.getProperty("KEYSTORE_PASS") ?: System.getenv("KEYSTORE_PASS")
+    val alias = properties.getProperty("KEY_ALIAS") ?: System.getenv("KEY_ALIAS")
+    val pwd = properties.getProperty("KEY_PASSWORD") ?: System.getenv("KEY_PASSWORD")
+    if (keystorePath != null) {
+        signingConfigs {
+            create("release") {
+                storeFile = file(keystorePath)
+                storePassword = keystorePwd
+                keyAlias = alias
+                keyPassword = pwd
+                enableV3Signing = true
+            }
+        }
+    }
+
     buildTypes {
         getByName("release") {
             isMinifyEnabled = true
             isShrinkResources = true
             proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+        }
+    }
+
+    flavorDimensionList.add("tier")
+    productFlavors {
+        create("CI") {
+            dimension = "tier"
+            versionCode = defaultConfig.versionCode?.plus(1)
+            versionName = "${defaultConfig.versionName?.split(" - ")?.get(0)}${getGitHeadRefsSuffix(rootProject)}"
+        }
+        create("Release-apk") {
+            dimension = "tier"
         }
     }
 
@@ -59,3 +91,27 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
 }
 
+// from https://github.com/qqlittleice/MiuiHome_R/blob/main/app/build.gradle.kts
+fun getGitHeadRefsSuffix(project: Project): String {
+    // .git/HEAD描述当前目录所指向的分支信息，内容示例："ref: refs/heads/master\n"
+    val headFile = File(project.rootProject.projectDir, ".git" + File.separator + "HEAD")
+    if (headFile.exists()) {
+        val string: String = headFile.readText(Charsets.UTF_8)
+        val string1 = string.replace(Regex("""ref:|\s"""), "")
+        val result = if (string1.isNotBlank() && string1.contains('/')) {
+            val refFilePath = ".git" + File.separator + string1
+            // 根据HEAD读取当前指向的hash值，路径示例为：".git/refs/heads/master"
+            val refFile = File(project.rootProject.projectDir, refFilePath)
+            // 索引文件内容为hash值+"\n"，
+            // 示例："90312cd9157587d11779ed7be776e3220050b308\n"
+            refFile.readText(Charsets.UTF_8).replace(Regex("""\s"""), "").subSequence(0, 7)
+        } else {
+            string.substring(0, 7)
+        }
+        println("commit_id: $result")
+        return "-CI.$result"
+    } else {
+        println("WARN: .git/HEAD does NOT exist")
+        return ""
+    }
+}

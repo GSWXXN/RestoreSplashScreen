633e9b8d7ddfd9b3bbe123fa919ca75a708e5b6a
GSWXXN <819269088@qq.com>
2023-04-26 01:27:22 -0400

    update BackgroundExcept.kt

--- app/src/main/java/com/gswxxn/restoresplashscreen/ui/configapps/BackgroundExcept.kt
+++ app/src/main/java/com/gswxxn/restoresplashscreen/ui/configapps/BackgroundExcept.kt
@@ -5,6 +5,7 @@ import com.gswxxn.restoresplashscreen.data.DataConst
 import com.gswxxn.restoresplashscreen.ui.`interface`.IConfigApps
 import com.highcapable.yukihookapi.hook.xposed.prefs.data.PrefsData
 
+// 背景 - 替换背景颜色 - 排除列表
 object BackgroundExcept :IConfigApps {
     override val titleID: Int
         get() = R.string.background_except_title

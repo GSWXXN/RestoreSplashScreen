4239cfcca419c1a9c243f151c6c790fb103d9adb
GSWXXN <34705923+GSWXXN@users.noreply.github.com>
2024-12-31 01:50:34 -0500

    添加注释

--- app/src/main/java/com/gswxxn/restoresplashscreen/ui/page/data/BackgroundPageData.kt
+++ app/src/main/java/com/gswxxn/restoresplashscreen/ui/page/data/BackgroundPageData.kt
@@ -3,6 +3,9 @@ package com.gswxxn.restoresplashscreen.ui.page.data
 import androidx.annotation.StringRes
 import com.gswxxn.restoresplashscreen.R
 
+/**
+ * 替换背景颜色 下拉框内容
+ */
 enum class ChangeBGColorTypes(@StringRes val stringID: Int) {
     NotChangeBGColor(R.string.not_change_bg_color),
     FromIcon(R.string.from_icon),
@@ -10,6 +13,9 @@ enum class ChangeBGColorTypes(@StringRes val stringID: Int) {
     FromCustom(R.string.from_custom)
 }
 
+/**
+ * 背景颜色模式 下拉框内容, 包括浅色/深色/跟随系统
+ */
 enum class BGColorModes(@StringRes val stringID: Int) {
     LightColor(R.string.light_color),
     DarkColor(R.string.dark_color),
